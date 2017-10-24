ed100bc685dd9c8b5fa43c73f3d73f7aeec89ee9
github username <github email address>
2017-10-24 09:24:10 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  limit_range = (1...limit).to_a
+  threes_and_fives = limit_range.select do |number|
+    number % 3 == 0 || number % 5 == 0
+  end
+  threes_and_fives
+end
+
+def sum_multiples(limit)
+  sum = collect_multiples(limit).inject(:+)
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    limit_range = (1...self.limit).to_a
+    threes_and_fives = limit_range.select do |number|
+      number % 3 == 0 || number % 5 == 0
+    end
+    threes_and_fives
+  end
+
+  def sum_multiples
+    sum = collect_multiples.inject(:+)
+    sum
+  end
+
+end
